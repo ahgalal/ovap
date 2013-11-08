e3acca4e91a31b9b87ddb16f7830fa9cd1cdcd6c
agalal <ceng.ahmedgalal@gmail.com>
2013-11-08 23:53:22 +0200

    issue #377 - ASSIGNED: Launching analysis session, synchronizing with streaming session http://agalaldev.info/dev/mantis/view.php?id=377
    - Adding EMF utils plugin

---
+++ plugins/ovap.emf.utils/src/ovap/emf/utils/EMFUtils.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package ovap.emf.utils;
+
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.emf.common.util.EMap;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.edit.domain.EditingDomain;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
+
+/**
+ * @author Creative
+ */
+public class EMFUtils {
+	private static HashMap<IProject, TransactionalEditingDomain>	map	= new HashMap<IProject, TransactionalEditingDomain>();
+
+	public static TransactionalEditingDomain getEditingDomain(
+			final IProject project) {
+		TransactionalEditingDomain transactionalEditingDomain = map
+				.get(project);
+		if (transactionalEditingDomain == null) {
+			transactionalEditingDomain = TransactionalEditingDomain.Factory.INSTANCE
+					.createEditingDomain(new ResourceSetImpl());
+			map.put(project, transactionalEditingDomain);
+		}
+		return transactionalEditingDomain;
+	}
+
+	public static Resource getEMFResource(final IResource wsResource) {
+		final EditingDomain editingDomain = getEditingDomain(wsResource
+				.getProject());
+		final ResourceSet resourceSet = editingDomain.getResourceSet();
+		final Resource resource = resourceSet.getResource(URI
+				.createPlatformResourceURI(wsResource.getFullPath().toString(),
+						true), true);
+		return resource;
+	}
+	
+	public static HashMap<String, String> getHashMap(final EMap<String, String> map) {
+		final HashMap<String, String> hashMap = new HashMap<String, String>();
+		for (final Entry<String, String> entry : map) {
+			hashMap.put(entry.getKey(), entry.getValue());
+		}
+		return hashMap;
+	}
+
+
+}
